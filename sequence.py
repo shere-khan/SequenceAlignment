fe105e75d5e7b9baa9a1975381ce359272c18703
Justin-PC\Justin <justin.barry.e>
2017-10-27 20:15:54 -0400

    adding test class

---
+++ sequence.py
@@ -0,0 +1,22 @@
+class StringTool:
+    def __init__(self):
+        pass
+
+    def __eq__(self, other):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def alignment(self, s1, s2):
+        pass
+
+    def __alignment(self, s1, s2):
+        pass
+
+
+class StringCreator:
+    pass
---
+++ sequencetest.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class TestStringAlignment(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
