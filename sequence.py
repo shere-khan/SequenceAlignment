54c3ea514b064fc84e68bdc38a7f3ded2c36c568
Justin-PC\Justin <justin.barry.e>
2017-10-28 22:01:38 -0400

    Implemented O(m) space O(mn) time alignemnt algorithm

--- sequence.py
+++ sequence.py
@@ -23,9 +23,9 @@ class StringTool:
                 val1 = self.A[i - 1][j - 1]
                 val2 = self.A[i][j - 1]
                 val3 = self.A[i - 1][j]
-                c1 = self.cost(s1[i - 1], s2[j - 1])
-                c2 = self.cost(s1[i - 1], "")
-                c3 = self.cost("", s2[j - 1])
+                c1 = self.__cost(s1[i - 1], s2[j - 1])
+                c2 = self.__cost(s1[i - 1], "")
+                c3 = self.__cost("", s2[j - 1])
                 a = val1 + c1
                 b = val2 + c2
                 c = val3 + c3
@@ -36,8 +36,25 @@ class StringTool:
 
         return self.A[len(s1)][len(s2)]
 
-    def cost(self, x, y):
+    def alignment_linear(self, s1, s2):
+        prev = [0] * (len(s1) + 1)
+        cur = []
+        for i in range(1, len(s2) + 1):
+            cur = [0]
+            for j in range(1, len(s1) + 1):
+                a = prev[j - 1] + self.__cost(s2[i - 1], s1[j - 1])
+                b = cur[-1] + self.__cost(s2[i - 1], "")
+                c = prev[j] + self.__cost("", s1[j - 1])
+                cur.append(min(a, b, c))
+
+            prev = cur
+
+        return cur[-1]
+
+    @staticmethod
+    def __cost(x, y):
         return 0 if x == y else 1
+
 
 class StringCreator:
     pass
--- sequencetest.py
+++ sequencetest.py
@@ -7,13 +7,17 @@ class TestStringAlignment(unittest.TestCase):
     def setUp(self):
         self.s = sequence.StringTool()
 
-    def test(self):
-        s1 = "actg"
-        s2 = "abta"
+    def test_alignment(self):
+        s1 = "actgaabxu"
+        s2 = "abtaaabxc"
         alignment = self.s.alignment(s1, s2)
         print(alignment)
-        # print(alignment[0])
-        # print(alignment[1])
+
+    def test_alignment_linear(self):
+        s1 = "actgaabxu"
+        s2 = "abtaaabxc"
+        alignment = self.s.alignment_linear(s1, s2)
+        print(alignment)
 
 
 if __name__ == '__main__':
