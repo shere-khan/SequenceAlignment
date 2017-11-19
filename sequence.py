8229606d9f1278cc65759abc098ab00f78238d95
Justin-PC\Justin <justin.barry.e>
2017-11-19 17:33:44 -0500

    Added functionality for aligning sequences from COT5405Project2

--- sequence.py
+++ sequence.py
@@ -1,8 +1,4 @@
 class StringTool:
-    def __init__(self):
-        # self.__prev = []
-        self.A = []
-
     def __eq__(self, other):
         pass
 
@@ -12,39 +8,16 @@ class StringTool:
     def __str__(self):
         pass
 
-    def alignment(self, s1, s2):
-        for i in range(len(s1) + 1):
-            self.A.append([])
-            for j in range(len(s2) + 1):
-                self.A[i].append(0)
-
-        for i in range(1, len(s1) + 1):
-            for j in range(1, len(s2) + 1):
-                val1 = self.A[i - 1][j - 1]
-                val2 = self.A[i][j - 1]
-                val3 = self.A[i - 1][j]
-                c1 = self.__cost(s1[i - 1], s2[j - 1])
-                c2 = self.__cost(s1[i - 1], "")
-                c3 = self.__cost("", s2[j - 1])
-                a = val1 + c1
-                b = val2 + c2
-                c = val3 + c3
-                # a = self.A[i - 1][j - 1] + self.cost(s1[i], s2[j])
-                # b = self.A[i][j - 1] + self.cost(s1[i], "")
-                # c = self.A[i - 1][j] + self.cost("", s2[j])
-                self.A[i][j] = min(a, b, c)
-
-        return self.A[len(s1)][len(s2)]
-
-    def alignment_linear(self, s1, s2):
+    @staticmethod
+    def alignment_linear(s1, s2):
         prev = [0] * (len(s1) + 1)
         cur = []
         for i in range(1, len(s2) + 1):
             cur = [0]
             for j in range(1, len(s1) + 1):
-                a = prev[j - 1] + self.__cost(s2[i - 1], s1[j - 1])
-                b = cur[-1] + self.__cost(s2[i - 1], "")
-                c = prev[j] + self.__cost("", s1[j - 1])
+                a = prev[j - 1] + StringTool.__cost(s2[i - 1], s1[j - 1])
+                b = cur[-1] + StringTool.__cost(s2[i - 1], "")
+                c = prev[j] + StringTool.__cost("", s1[j - 1])
                 cur.append(min(a, b, c))
 
             prev = cur
@@ -52,8 +25,103 @@ class StringTool:
         return cur[-1]
 
     @staticmethod
+    def alignment(s1, s2, f):
+        n = len(s1)
+        m = len(s2)
+        M = []
+        StringTool.populate_base(M, m, n)
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                a = M[i - 1][j - 1][0] + f(s1[i - 1], s2[j - 1])
+                b = M[i - 1][j][0] + f("", s2[j - 1])
+                c = M[i][j - 1][0] + f(s1[i - 1], "")
+                vals = [a, b, c]
+                mynn = min(vals)
+                M[i][j] = (mynn, StringTool.get_parent(vals.index(mynn), i, j))
+
+        return M
+
+    @staticmethod
+    def populate_base(M, m, n):
+        for i in range(0, n + 1):
+            for j in range(0, m + 1):
+                if i == 0 and j == 0:
+                    M.append([(i, None)])
+                elif i == 0 and j > 0:
+                    M[i].append((j, (i, j - 1, 'left')))
+                elif i > 0 and j == 0:
+                    M.append([(i, (i - 1, j, 'up'))])
+                else:
+                    M[i].append((0, None))
+
+    @staticmethod
+    def get_parent(val, i, j):
+        if val == 0:
+            return i - 1, j - 1, 'diag'
+        if val == 1:
+            return i - 1, j, 'up'
+        if val == 2:
+            return i, j - 1, 'left'
+
+    @staticmethod
     def __cost(x, y):
         return 0 if x == y else 1
+
+    @staticmethod
+    def unpack_alignment(M, s1, s2, r1, r2):
+        if M is None:
+            return 'No alignment'
+        n = len(s1)
+        m = len(s2)
+        parent_info = M[n][m][1]
+        if parent_info is None:
+            return 'No alignment'
+
+        parent_i = parent_info[0]
+        parent_j = parent_info[1]
+        res = StringTool.get_string(s1, s2, parent_info[2], n, m)
+        r1 += res[0]
+        r2 += res[1]
+
+        r1, r2 = StringTool.__unpack_alignment(M, parent_i, parent_j, s1, s2, r1, r2)
+        return r1, r2
+
+    @staticmethod
+    def __unpack_alignment(M, i, j, s1, s2, r1, r2):
+        parent_info = M[i][j][1]
+        if parent_info is None:
+            return r1, r2
+        if parent_info[0] == parent_info[1] == 0:
+            if parent_info[2] == 'up':
+                r1 = s1[i - 1] + r1
+                r2 = "_" + r2
+                return r1, r2
+
+            r1 = "_" + r1
+            r2 = s2[j - 1] + r2
+            return r1, r2
+
+        res = StringTool.get_string(s1, s2, parent_info[2], i, j)
+        r1 = res[0] + r1
+        r2 = res[1] + r2
+
+        parent_i = parent_info[0]
+        parent_j = parent_info[1]
+        parent = M[parent_i][parent_j][1]
+
+        r1, r2 = StringTool.__unpack_alignment(M, parent_i, parent_j, s1, s2, r1, r2)
+        return r1, r2
+
+    @staticmethod
+    def get_string(s1, s2, s, i, j):
+        if s == 'diag':
+            return s1[i - 1], s2[j - 1]
+        if s == 'up':
+            # return s1[i - 1], s2[j]
+            return s1[i - 1], "_"
+        if s == 'left':
+            # return s1[i], s2[j - 1]
+            return "_", s2[j - 1]
 
 
 class StringCreator:
--- sequencetest.py
+++ sequencetest.py
@@ -7,18 +7,31 @@ class TestStringAlignment(unittest.TestCase):
     def setUp(self):
         self.s = sequence.StringTool()
 
-    def test_alignment(self):
-        s1 = "actgaabxu"
-        s2 = "abtaaabxc"
-        alignment = self.s.alignment(s1, s2)
-        print(alignment)
-
     def test_alignment_linear(self):
-        s1 = "actgaabxu"
-        s2 = "abtaaabxc"
-        alignment = self.s.alignment_linear(s1, s2)
+        s1 = "ACTGAABXUAXCY"
+        s2 = "RABTAAABXCAAACZQQ"
+        alignment = sequence.StringTool.alignment_linear(s1, s2)
         print(alignment)
 
+    def test_alignment(self):
+        # s1 = 'AGGCTATCACCTGACCTCCAGGCCGATGCCC'
+        # s2 = 'TAGCTATCACGACCGCGGTCGATTTGCCCGAC'
+
+        s1 = 'ACTGAABXUAXCY'
+        s2 = 'RABTAAABXCAAACZQQ'
+
+        f = lambda x, y: 1 if y == "" or x == "" or x != y else 0
+
+        align = sequence.StringTool.alignment(s1, s2, f)
+
+        r1 = ""
+        r2 = ""
+        r1, r2 = sequence.StringTool.unpack_alignment(align, s1, s2, r1, r2)
+
+        print('Edit Distance: ' + str(align[-1][-1][0]))
+        print('s1: ' + r1)
+        print('s2: ' + r2)
+        print()
 
 if __name__ == '__main__':
     unittest.main()
