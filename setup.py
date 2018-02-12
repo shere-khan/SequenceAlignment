8a157964a0337e853140e5f698e4656f5f4a68ee
Unknown <justin.barry.e@gmail.com>
2018-02-12 02:22:00 -0500

    update gitignore for env/

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sample-app',
+    version='1.0.0',
+    description='Step 1 tut',
+    url='',
+    author='jb',
+    packages=find_packages()
+)
